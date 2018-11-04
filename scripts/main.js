216b204cefaf3e95f3cdc815a0fd8eb9b602f432
Nicopok <pro.nicolasfernandez@gmail.com>
2018-11-04 21:02:50 +0100

    jour3

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function redirection () {
+let reponse = confirm("Veux-tu aller sur ce beau site xxxvidsxxx ? :)");
+if (reponse == true) {
+	open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+} else {
+	 ;
+}
+}
+
+setTimeout (redirection, 1000);
